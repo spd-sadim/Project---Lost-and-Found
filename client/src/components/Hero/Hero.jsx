47778223940422e83971dd9fd7aea26fe508df5e
Sadim Mali <sadimmalakar77@gmail.com>
2024-03-01 19:12:26 +0545

    feat: Add Hero component with search functionality

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./hero.css";
 
 export default function Hero() {
   return (
-    <div className='py-5'>Hero</div>
-  )
+    <section className="hero">
+      <Container>
+        <div className="hero-content text-center py-5">
+          <h1>Bhetayoo Lost & found solutions</h1>
+          <p>
+            "Our website is dedicated to simplifying the process of reuniting
+            lost belongings with their owners.
+          </p>
+        <div className="search w-100 d-flex justify-content-center px-5 pt-3 position-relative">
+          <input type="text" name="search" className="w-50 py-3 px-2 rounded" />
+          <button>Check</button>
+        </div>
+        </div>
+      </Container>
+    </section>
+  );
 }
