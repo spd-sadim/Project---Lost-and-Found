0b6ff795489abc488e08140b523bd64b34bddd1f
Sadim Mali <sadimmalakar77@gmail.com>
2024-03-01 19:16:54 +0545

    feat: Add Annoucement.jsx component in Home.jsx page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Announcement from "../components/Announcement/Announcement";
 import Hero from "../components/Hero/Hero";
 
 export default function Home() {
   return (
     <div>
         <Hero />
+        <Announcement />
     </div>
   )
 }
