4b3434ac9a5bcf22712ab89fc4835677eb062d7c
Sadim Mali <sadimmalakar77@gmail.com>
2024-03-02 20:05:15 +0545

    refactor: Add max-width to card and dynamic card for item detail

--- client/src/components/Announcement/AnnouncementCard.jsx
+++ client/src/components/Announcement/AnnouncementCard.jsx
@@ -1,43 +1,37 @@
 import Card from "react-bootstrap/Card";
-import image from "../../assets/samsung.jpg";
 
-export default function AnnouncementCard() {
-  const item = {
-    id: "1",
-    name: "Samsung s21",
-    category: "mobile",
-    img: image,
-    location: "Kiritipur",
-    status: "Found",
-    date: "2024-02-40",
-  };
+export default function AnnouncementCard({ item }) {
   return (
     <Card
-      className="position-relative"
+      className="position-relative mw-100"
       style={{ width: "18rem", backgroundColor: "#eff1f4" }}
     >
-      <Card.Img variant="top" src={image} className="p-2 rounded" />
+      <Card.Img variant="top" src={item.img} className="p-2 rounded" />
       <span
-        className="position-absolute rounded px-3 py-1 fw-bold"
+        className="position-absolute px-3 py-1 fw-bold"
         style={{
           top: "15px",
           left: "15px",
           backgroundColor: "white",
-          color: "blue",
+          color: item.status == "Found" ? "blue" : "#ed6854",
           border: "1px solid gray",
-          opacity: ".6",
+          borderRadius: "20px",
+          opacity: ".7",
         }}
       >
-        {item.status}
+       {item.status}
       </span>
       <Card.Body>
         <Card.Title>
-          <div className="d-flex justify-content-between primary-color">{item.name}</div>
+          <div className="d-flex justify-content-between primary-color">
+            {item.name}
+          </div>
         </Card.Title>
         <Card.Text>
-          <p className="primary-color">{item.date}</p>
+          <span className="d-block">{item.location}</span>
+          {item.date}
         </Card.Text>
-        <button type="button">Details</button>
+        <button type="button p-2">Details</button>
       </Card.Body>
     </Card>
   );
