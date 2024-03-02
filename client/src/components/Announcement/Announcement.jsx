5a8e2364d843f053891601b8ca3a1108fcdf0682
Sadim Mali <sadimmalakar77@gmail.com>
2024-03-02 20:05:39 +0545

    feat: Add dummy array to map the AnnoucementCard.jsx

--- client/src/components/Announcement/Announcement.jsx
+++ client/src/components/Announcement/Announcement.jsx
@@ -1,13 +1,59 @@
 import AnnouncementCard from "./AnnouncementCard";
-import {Container} from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap";
+import image from "../../assets/samsung.jpg";
+
 export default function Announcement() {
+  const items = [
+    {
+      id: 1,
+      name: "Samsung s21",
+      category: "mobile",
+      img: image,
+      location: "Kiritipur",
+      status: "Found",
+      date: "2024-02-40",
+    },
+    {
+      id: 2,
+      name: "Samsung s21",
+      category: "mobile",
+      img: image,
+      location: "Kiritipur",
+      status: "Found",
+      date: "2024-02-40",
+    },
+    {
+      id: 3,
+      name: "Samsung s21",
+      category: "mobile",
+      img: image,
+      location: "Kiritipur",
+      status: "Lost",
+      date: "2024-02-40",
+    },
+    {
+      id: 4,
+      name: "Samsung s21",
+      category: "mobile",
+      img: image,
+      location: "Kiritipur",
+      status: "Found",
+      date: "2024-02-40",
+    },
+  ];
   return (
     <div className="py-2">
       <Container>
-        <div className="title">
+        <div className="title py-5">
           <h3>Announcement</h3>
         </div>
-        <AnnouncementCard />
+        <Row gap={8}>
+          {items.map((item) => (
+            <Col lg={3} md={4} key={item.id}>
+              <AnnouncementCard item={item} />
+            </Col>
+          ))}
+        </Row>
       </Container>
     </div>
   );
