1cfdfeb352bf440007ea86a1e2b6f42641d2cc2b
Sadim Mali <sadimmalakar77@gmail.com>
2024-03-06 20:00:08 +0545

    style: Overlay & hero component

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,25 +1,23 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import "./hero.css";
+import HeroLeft from "./HeroLeft";
+import HeroForm from "./HeroForm";
 
 export default function Hero() {
   return (
-    <section className="hero">
-      <Container>
-        <div className="hero-content text-center py-5">
-          <h1>Bhetayoo Lost & found solutions</h1>
-          <p>
-            "Our website is dedicated to simplifying the process of reuniting
-            lost belongings with their owners."
-          </p>
-          <div className="search w-100 d-flex justify-content-center px-5 pt-3 position-relative">
-            <input
-              type="text"
-              name="search"
-              className="w-50 py-3 px-2 rounded"
-            />
-            <button>Check</button>
-          </div>
+    <section className="hero position-relative">
+        <div className="overlay w-100 h-100"></div>
+      <Container className="h-100">
+        <div className="hero-content  py-5 position-relative h-100">
+          <Row className="align-items-center h-100">
+            <Col lg={7}>
+              <HeroLeft />
+            </Col>
+            <Col lg={5}>
+              <HeroForm />
+            </Col>
+          </Row>
         </div>
       </Container>
     </section>
