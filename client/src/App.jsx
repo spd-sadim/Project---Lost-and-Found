068632c7f4e8f858e8310cb71f6d619307722e93
Sadim Mali <sadimmalakar77@gmail.com>
2024-03-07 17:57:49 +0545

    feat: Add route for ReportItem component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import HelpAdvice from "./pages/help-advice/HelpAdvice";
+import ReportItem from "./pages/report-item/ReportItem";
 
 export default function App() {
   return (
@@ -14,7 +15,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/help-advice" element={<HelpAdvice />} />
-
+        <Route path="/report-item" element={<ReportItem />} />
       </Routes>
     </>
   );
