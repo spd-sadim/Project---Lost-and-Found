f90f9bce5f3c634bb85ee0a1ed23fe82ba32c916
Sadim Mali <sadimmalakar77@gmail.com>
2024-03-07 17:54:20 +0545

    Add HowToWork.jsx in Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Announcement from "../components/Announcement/Announcement";
 import Hero from "../components/Hero/Hero";
+import HowItWorks from "../components/How-it-works/HowItWorks";
 
 export default function Home() {
   return (
     <div>
         <Hero />
         <Announcement />
+        <HowItWorks />
     </div>
   )
 }
