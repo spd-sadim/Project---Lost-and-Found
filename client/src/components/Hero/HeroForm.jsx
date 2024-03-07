bf2258b4c34e432af27c1bcf5a8db032381c5428
Sadim Mali <sadimmalakar77@gmail.com>
2024-03-07 17:49:04 +0545

    feat: Add state to send selected category as query params

--- client/src/components/Hero/HeroForm.jsx
+++ client/src/components/Hero/HeroForm.jsx
@@ -1,7 +1,10 @@
-import { Button } from "react-bootstrap";
 import { categories } from "../category";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function HeroForm() {
+  const [categoryValue, setCategoryValue] = useState("all");
+
   return (
     <div className="d-flex flex-column w-100" >
       {/* <label>What was lost or found *</label>
@@ -9,13 +12,15 @@ export default function HeroForm() {
       <label>Categories of item</label>
       {/* <input list="categories" placeholder="Search" /> */}
       {/* <datalist id="categories" className="h-50"> */}
-      <select name="category" className="p-2">
-      <option defaultValue={"All category"}>All category</option>
+      <select name="category" className="p-2" onChange={(e)=> { setCategoryValue(e.target.value)}}>
+      <option defaultValue={"all"}>All category</option>
         {categories.map((category, index) => (
           <option value={category} key={index}> {category} </option>
         ))}
       </select>
-          <Button className="mt-3 py-2"> Search</Button>
+      <Link to={`/view-item?category=${categoryValue}`} className="w-100">
+          <button className="text-white myBtn-primary mt-3 py-2  w-100"> Search</button>
+      </Link>
     </div>
   );
 }
