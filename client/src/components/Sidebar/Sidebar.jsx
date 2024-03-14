ec2b356763048fb301a44350df4ebf0cb92a8c5e
Sadim Mali <sadimmalakar77@gmail.com>
2024-03-14 19:02:54 +0545

    feat: Create a constant and add sidebar links

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,63 +1,117 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Sidebar() {
+  const [activeDropdown, setActiveDropdown] = useState(null);
+
+  //fn that set activeDropdown value as id
+  const handleDropDown = (id) => {
+    console.log(id);
+    console.log(activeDropdown);
+    setActiveDropdown(activeDropdown === id ? null : id);
+  };
+
   const items = [
-    {
-      id: 1,
-      name: "View Site",
-      route: "/",
-    },
     {
       id: 2,
       name: "Manage Found Item",
-      subLinks : [
+      subLinks: [
         {
+          id: 1,
           subName: "View Found Item",
-          subRoute: "/view-found-item"
+          subRoute: "/view-item?v=found",
         },
         {
+          id: 2,
           subName: "Add Found Item",
-          subRoute: "/add-found-item"
+          subRoute: "/add-found-item",
         },
-      ]
+      ],
     },
     {
       id: 3,
       name: "Manage Lost Item",
-      subLinks : [
+      subLinks: [
         {
+          id: 1,
           subName: "View Lost Item",
-          subRoute: "/view-lost-item"
+          subRoute: "/view-item?v=lost",
         },
         {
+          id: 2,
           subName: "Add Lost Item",
-          subRoute: "/add-lost-item"
+          subRoute: "/add-lost-item",
         },
-      ]
+      ],
     },
     {
       id: 4,
-      name: "Account setting",
-      route: "/account-setting",
+      name: "Account Setting",
+      subLinks: [
+        {
+          id: 1,
+          subName: "User Profile",
+          subRoute: "/user",
+        },
+        {
+          id: 2,
+          subName: "Change Password",
+          subRoute: "/change-password",
+        },
+      ],
     },
     {
       id: 5,
       name: "Notification",
-      route: "/notification",
+      subLinks: [
+        {
+          id: 1,
+          subName: "Lost Notification",
+          subRoute: "/lost/notification",
+        },
+        {
+          id: 2,
+          subName: "Found Notification",
+          subRoute: "/found/notification",
+        },
+      ],
     },
-  ]
+  ];
   return (
-    <div>
-        <div className="sidebar-links">
-          <ul className='d-flex flex-column list-unstyled'>
-            <Link to="/" className='text-decoration-none'><li className='text-white'>View site</li></Link>
-            <Link to="/dashboard/view-item" className='text-decoration-none'><li className='text-white' >Manage Found Item</li></Link>
-            <Link to="/" className='text-decoration-none'><li className='text-white' >Manage Lost Item</li></Link>
-            <Link to="/dashboard/account-setting" className='text-decoration-none'><li  className='text-white'>Account setting</li></Link>
-            <Link to="/dashboard/notification" className='text-decoration-none'><li className='text-white' >Notification</li></Link>
-          </ul>
-        </div>
-    </div>
-  )
+    <aside
+      className="sidebar-container h-100"
+      style={{ width: "250px", backgroundColor: "red" }}
+    >
+      <div className="sidebar-links px-3">
+        <ul className="d-flex flex-column list-unstyled">
+          <Link to="/" className="text-decoration-none">
+            <li className="text-white">View site</li>
+          </Link>
+          {items.map((item) => (
+            <li
+              key={item.id}
+              className={activeDropdown === item.id ? "text-" : "text-white"}
+              onClick={() => {
+                handleDropDown(item.id);
+              }}
+            >
+              {item.name}
+
+              <ul className={activeDropdown === item.id ? "d-block" : "d-none"}>
+                {item.subLinks.map((subLink) => (
+                  <Link
+                    to={`/dashboard${subLink.subRoute}`}
+                    className="text-decoration-none"
+                    key={subLink.id}
+                  >
+                    <li className="text-white">{subLink.subName}</li>
+                  </Link>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </aside>
+  );
 }
--- client/src/pages/dashboard/UserDashboard.jsx
+++ client/src/pages/dashboard/UserDashboard.jsx
@@ -4,7 +4,7 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 export default function UserDashboard() {
   return (
     <div className="main d-flex">
-      <div className="sidebar-container h-100" style={{width: "250px", backgroundColor: "red"}}>
+      <div>
         <Sidebar />
       </div>
       <div className="content-container pt-5 mt-5">
