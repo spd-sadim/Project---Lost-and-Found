a9a0441db653d62fcdbb99e6234b70303be39c2a
Sadim Mali <sadimmalakar77@gmail.com>
2024-03-31 22:23:15 +0545

    feat: Add map iframe

--- client/src/pages/help-advice/HelpAdvice.jsx
+++ client/src/pages/help-advice/HelpAdvice.jsx
@@ -1,9 +1,23 @@
 import HelpAdviceForm from "../../components/Help-advice/HelpAdviceForm";
+import Footer from "../../components/footer/Footer";
 
 export default function HelpAdvice() {
   return (
     <section>
       <HelpAdviceForm />
+      {/* map */}
+      <iframe
+        className="mt-5"
+        width="100%"
+        height="400"
+        allowFullScreen=""
+        loading="lazy"
+        id="gmap_canvas"
+        samesite="Strict"
+        src="https://maps.google.com/maps?q=kirtipur&t=&z=10&ie=UTF8&iwloc=&output=embed"
+        frameborder="0"
+      ></iframe>  
+      <Footer />
     </section>
   )
 }
