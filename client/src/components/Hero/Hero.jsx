72e2ae3e01cc40524bc1ef85c432a154a59fd796
Sadim Mali <sadimmalakar77@gmail.com>
2024-03-31 22:25:12 +0545

    refactor: Remove component and create Hero component

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,23 +1,25 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import "./hero.css";
-import HeroLeft from "./HeroLeft";
 import HeroForm from "./HeroForm";
 
 export default function Hero() {
   return (
     <section className="hero position-relative">
-        <div className="overlay w-100 h-100"></div>
       <Container className="h-100">
-        <div className="hero-content  py-5 position-relative h-100">
-          <Row className="align-items-center h-100">
-            <Col lg={7}>
-              <HeroLeft />
-            </Col>
-            <Col lg={5}>
-              <HeroForm />
-            </Col>
-          </Row>
+        <div className="hero-wrapper h-100 py-5 position-relative ">
+        <div className="hero-content text-center py-5">
+          <h1>
+            Reuniting lost belongings with their
+            owners.
+          </h1>
+          <p className="font-sec">
+            Browse lost and found items from our vast
+            network of over 11,500 diligent finders
+          </p>
+        </div>
+
+          <HeroForm />
         </div>
       </Container>
     </section>
