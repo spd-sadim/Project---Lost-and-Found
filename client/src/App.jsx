1154315238d9635f16dc71cf897d9ce09abcf32b
Sadim Mali <sadimmalakar77@gmail.com>
2024-04-02 22:27:34 +0545

    feat: Add SignInUp page as passed SignUp & Login component as children to their respective routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from "react-router";
 import NavbarNav from "./components/navbar/NavbarNav";
 import Home from "./pages/Home";
-import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/SignUp";
 import HelpAdvice from "./pages/help-advice/HelpAdvice";
 import ReportItem from "./pages/report-item/ReportItem";
 import UserDashboard from "./pages/dashboard/UserDashboard";
 import ViewItem from "./components/ViewItem";
 import DetailedView from "./components/DetailedView";
 import AddItem from "./components/AddItem";
+import SignInUp from "./pages/sign-in-up/SignInUp";
+import Login from "./components/login/Login";
+import SignUp from "./components/signup/SignUp";
 
 export default function App() {
   return (
@@ -16,8 +17,8 @@ export default function App() {
       {/* <NavbarNav /> */}
       <Routes>
         <Route index path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<SignInUp><Login /> </SignInUp>} />
+        <Route path="/sign-up" element={<SignInUp><SignUp /> </SignInUp>} />
         <Route path="/help-advice" element={<HelpAdvice />} />
         <Route path="/report-item" element={<ReportItem />} />
         <Route path="/dashboard" element={<UserDashboard />}>
