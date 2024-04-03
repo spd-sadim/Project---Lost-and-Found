9171daf81ebaabc2133b86cf6ada026f3889d480
Sadim Mali <sadimmalakar77@gmail.com>
2024-04-03 21:22:04 +0545

    feat: Establish connection to postgreSQL database

--- api/db.js
+++ api/db.js
@@ -1,12 +1,12 @@
-import { Pool } from "pg";
+import pg from "pg";
 import "dotenv/config";
 
-const pool = new Pool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+const pool = new pg.Pool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT,
 });
 
 export default pool;
