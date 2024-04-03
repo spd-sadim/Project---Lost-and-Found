6ee372c0b68429bce6610eee218b799691e4631f
Sadim Mali <sadimmalakar77@gmail.com>
2024-04-03 21:24:06 +0545

    feat: Create controller that getUsers from database

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,11 @@
-export const test = (req, res)=>{
-    res.json({message: "Hello world"});
+import pool from "../db.js"
+
+export const getUsers = async(req, res)=>{
+   try{
+    const result = await pool.query("SELECT * FROM users");
+    res.json({message: result});
+   } catch (err) {
+            res.status(500).json({error: err});
+       }
+        // res.json({message: "hello"});
 }
