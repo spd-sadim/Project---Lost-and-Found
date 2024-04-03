bd4713a8256f922690da28e11149f984a95b6fe1
Sadim Mali <sadimmalakar77@gmail.com>
2024-04-03 21:23:30 +0545

    feat: Add getUsers controller in user.route.js

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+import { getUsers } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/", test);
+router.get("/", getUsers);
 
 export default router;
