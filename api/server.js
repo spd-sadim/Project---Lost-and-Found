49bfff58ac1ffb0685eb38513e87eb8f03161d15
Sadim Mali <sadimmalakar77@gmail.com>
2024-04-04 18:45:32 +0545

    feat: Add cookieParser  middleware to create cookie

--- api/server.js
+++ api/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
