4e22a6399cdf99838e4c24530a506dce67d652d4
Sadim Mali <sadimmalakar77@gmail.com>
2024-04-04 13:43:43 +0545

    feat: Add middleWare router for authentication

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 const port = 3000;
@@ -7,6 +8,7 @@ const port = 3000;
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server started at port ${port}`);
