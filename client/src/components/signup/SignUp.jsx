3589b8387029e6be148035218a8eee5db0078992
Sadim Mali <sadimmalakar77@gmail.com>
2024-04-05 21:40:25 +0545

    feat: Integrate Login.jsx with API for SignUp functionality

--- client/src/components/signup/SignUp.jsx
+++ client/src/components/signup/SignUp.jsx
@@ -1,7 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Icon } from "@iconify-icon/react";
+import { useContext, useState } from "react";
+import axios from "axios";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function SignUp() {
+  const [credentials, setCredentials] = useState({
+    firstname: undefined,
+    lastname: undefined,
+    phonenumber: undefined,
+    email: undefined,
+    password: undefined,
+  });
+
+  const { loading, error, dispatch } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch({ type: "loginStart" });
+      const res = await axios.post("/api/auth/signup", credentials);
+      dispatch({ type: "signUpSuccess" });
+      console.log(res.data);
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+      dispatch({ type: "loginFailure", payload: err.response.data });
+    }
+  };
   return (
     <div
       className="p-3 mx-auto"
@@ -27,10 +58,11 @@ export default function SignUp() {
             </label>
             <input
               type="text"
-              name="fName"
+              name="firstname"
               placeholder="First Name"
               autoFocus
               className="py-3 px-2 rounded border w-100"
+              onChange={handleChange}
             />
           </div>
           <div>
@@ -39,10 +71,10 @@ export default function SignUp() {
             </label>
             <input
               type="text"
-              name="lName"
+              name="lastname"
               placeholder="Last Name"
-              autofocus
               className="py-3 px-2 rounded border w-100"
+              onChange={handleChange}
             />
           </div>
         </div>
@@ -53,10 +85,10 @@ export default function SignUp() {
 
         <input
           type="number"
-          name="number"
+          name="phonenumber"
           placeholder="Number"
-          autofocus
           className="py-3 px-2 rounded border"
+          onChange={handleChange}
         />
         <label>
           Email address <b className="text-danger">*</b>{" "}
@@ -66,6 +98,7 @@ export default function SignUp() {
           name="email"
           placeholder="Email"
           className=" py-3 px-2 rounded border"
+          onChange={handleChange}
         />
 
         <label>
@@ -73,18 +106,22 @@ export default function SignUp() {
         </label>
         <input
           type="password"
-          name="pwd"
+          name="password"
           placeholder="New password"
           className="px-2 py-3 rounded border"
+          onChange={handleChange}
         />
         <button
           type="button"
           className="p-3 rounded"
+          disabled={loading}
           style={{ backgroundColor: "#376679", color: "white" }}
+          onClick={handleClick}
         >
-          Log In
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
+      {error && <span className="text-danger">{error}</span>}
     </div>
   );
 }
