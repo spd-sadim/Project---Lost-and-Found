df99651651a6be40cc002d77c4326390d5a150cf
Sadim Mali <sadimmalakar77@gmail.com>
2024-04-05 21:32:53 +0545

    feat(auth): Implement AuthContext and AuthReducer - Implement AuthContext using createContext from React - Initialize state with user data fetched from localStorage or null - Define AuthReducer to handle various authentication actions - Actions include loginStart, signUpSuccess, loginSuccess, loginFailure, logout - Utilize localStorage to persist user data across sessions

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,70 @@
+import { createContext, useEffect, useReducer } from "react";
+
+const initialState = {
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  loading: false,
+  error: null,
+};
+
+export const AuthContext = createContext(initialState);
+
+const AuthReducer = (state, action) => {
+  switch (action.type) {
+    case "loginStart":
+      return {
+        user: null,
+        loading: true,
+        error: null,
+      };
+
+    case "signUpSuccess":
+      return {
+        user: null,
+        loading: false,
+        error: null,
+      };
+
+    case "loginSuccess":
+      return {
+        user: action.payload,
+        loading: false,
+        error: null,
+      };
+
+    case "loginFailure":
+      return {
+        user: null,
+        loading: false,
+        error: action.payload,
+      };
+
+    case "logout":
+      return {
+        user: null,
+        loading: false,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthReducer, initialState);
+  useEffect(()=>{
+    localStorage.setItem("user", JSON.stringify(state.user));
+  })
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        loading: state.loading,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
