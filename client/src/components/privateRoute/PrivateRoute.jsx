f6acdeac32335b4d02aa0e5a8aa84914f6b5d620
Sadim Mali <sadimmalakar77@gmail.com>
2024-04-06 13:56:52 +0545

    feat: Implement PrivateRoute component for role-based access control

--- client/src/components/privateRoute/PrivateRoute.jsx
+++ client/src/components/privateRoute/PrivateRoute.jsx
@@ -1,8 +1,16 @@
 import { useContext } from "react";
-import { Navigate, Outlet } from "react-router";
+import { Navigate, Outlet, useLocation } from "react-router";
 import { AuthContext } from "../../context/AuthContext";
 
-export default function PrivateRoute() {
-  const {user} = useContext(AuthContext);
-  return user ? <Outlet /> : <Navigate to={"/login"} />;
+export default function PrivateRoute({ allowedRoles }) {
+  const location = useLocation();
+  const { user } = useContext(AuthContext);
+  
+  return user.role === allowedRoles ? (
+    <Outlet />
+  ) : user ? (
+    <Navigate to={"/unauthorized"} state={{ from: location }} replace />
+  ) : (
+    <Navigate to={"/login"} state={{ from: location }} replace />
+  );
 }
