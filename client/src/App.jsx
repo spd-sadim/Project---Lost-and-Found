c4bb1a510001295c3d82bad895d35085736a4d23
Sadim Mali <sadimmalakar77@gmail.com>
2024-04-06 14:00:20 +0545

    feat: Manage PrivateRoute for dashboards and add unAuthorized and NotFound routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import NavbarNav from "./components/navbar/NavbarNav";
 import Home from "./pages/Home";
 import HelpAdvice from "./pages/help-advice/HelpAdvice";
 import ReportItem from "./pages/report-item/ReportItem";
-import UserDashboard from "./pages/dashboard/UserDashboard";
 import ViewItem from "./components/ViewItem";
 import DetailedView from "./components/DetailedView";
 import AddItem from "./components/AddItem";
@@ -11,6 +10,9 @@ import SignInUp from "./pages/sign-in-up/SignInUp";
 import Login from "./components/login/Login";
 import SignUp from "./components/signup/SignUp";
 import PrivateRoute from "./components/privateRoute/PrivateRoute";
+import Dashboard from "./pages/dashboard/Dashboard";
+import UnAuthorized from "./components/UnAuthorized";
+import NotFound from "./NotFound";
 
 export default function App() {
   return (
@@ -22,13 +24,21 @@ export default function App() {
         <Route path="/sign-up" element={<SignInUp><SignUp /> </SignInUp>} />
         <Route path="/help-advice" element={<HelpAdvice />} />
         <Route path="/report-item" element={<ReportItem />} />
-        <Route element={<PrivateRoute />}>
-        <Route path="/dashboard" element={<UserDashboard />}>
-          <Route path="/dashboard/view-item" element={<ViewItem />} />
-          <Route path="/dashboard/view-item/:id" element={<DetailedView />} />
-          <Route path="/dashboard/add-item" element={<AddItem />} />
+        <Route path="/unauthorized" element={<UnAuthorized />} />
+        {/* user dashboard */}
+        <Route element={<PrivateRoute allowedRoles={"user"}/>}>
+        <Route path="/user" element={<Dashboard />}>
+          <Route path="/user/view-item" element={<ViewItem />} />
+          <Route path="/user/view-item/:id" element={<DetailedView />} />
+          <Route path="/user/add-item" element={<AddItem />} />
+        </Route>
+        {/* admin dashboard */}
+        <Route element={<PrivateRoute allowedRoles={"admin"} />}>
+        <Route path="/admin" element={<Dashboard />} />
         </Route>
         </Route>
+        {/* 404 not found */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
