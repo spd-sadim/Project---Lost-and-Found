9c6e9ef9bb22dd83d6bcb90594fc81888a1c91a3
Sadim Mali <sadimmalakar77@gmail.com>
2024-04-06 13:51:24 +0545

    refactor: Rename Dashboard name to reuse component

---
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,24 @@
+import { Icon } from "@iconify-icon/react";
+import { useState } from "react";
+import { Outlet } from "react-router";
+import Sidebar from "../../components/Sidebar/Sidebar";
+
+export default function Dashboard({type}) {
+  const [isCollapse, setIsCollapse] = useState(false);
+  return (
+    <div className="wrapper d-flex overflow-hidden">
+      <Sidebar isCollapse={isCollapse} userType={type} />
+      <main className="container-fluid content-container mt-2">
+        <Icon
+          icon="jam:menu"
+          style={{ fontSize: "3.4rem" }}
+          onClick={() => {
+            setIsCollapse(!isCollapse);
+          }}
+        />
+
+        <Outlet />
+      </main>
+    </div>
+  );
+}
