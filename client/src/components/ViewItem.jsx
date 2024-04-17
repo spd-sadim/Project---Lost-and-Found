3579d7f104c506dad037041448755a93d3a9b4db
Sadim Mali <sadimmalakar77@gmail.com>
2024-04-17 20:40:33 +0545

    feat: react-router link to naviagate between pages

--- client/src/components/ViewItem.jsx
+++ client/src/components/ViewItem.jsx
@@ -1,43 +1,52 @@
 import Table from "react-bootstrap/Table";
 import { Icon } from "@iconify/react";
+import { Link } from "react-router-dom";
 
 export default function ViewItem() {
   return (
     <div>
-        <div className=" d-flex justify-content-between w-100 mb-3">
-          <h4>View Lost Items</h4>
-          <button className="btn btn-dark">+ New Item</button>
-        </div>
-        <Table responsive bordered hover size="lg">
-          <thead>
-            <tr>
-              <th>Item-ID</th>
-              <th>Item name</th>
-              <th>Category</th>
-              <th>Date lost</th>
-              <th>Location lost</th>
-              <th>Status</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Samsung</td>
-              <td>Mobile phone</td>
-              <td>2024-02-22</td>
-              <td>Barcelona, Spain</td>
-              <td>
-                <button className="btn btn-secondary">Reported</button>
-              </td>
-              <td className="d-flex gap-2">
-                <Icon icon="lets-icons:view-alt" />
+      <div className=" d-flex justify-content-between w-100 mb-3">
+        <h4>View Lost Items</h4>
+        <button className="btn btn-dark">+ New Item</button>
+      </div>
+      <Table responsive bordered hover size="lg">
+        <thead>
+          <tr>
+            <th>Item-ID</th>
+            <th>Item name</th>
+            <th>Category</th>
+            <th>Date lost</th>
+            <th>Location lost</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Samsung</td>
+            <td>Mobile phone</td>
+            <td>2024-02-22</td>
+            <td>Barcelona, Spain</td>
+            <td>
+              <button className="btn btn-secondary">Reported</button>
+            </td>
+            <td>
+              <div className="d-flex gap-2">
+              <Link to={`/user/view-item/dfaf`}>
+              <Icon icon="lets-icons:view-alt" />
+              </Link> 
+              <Link>
                 <Icon icon="tabler:edit" />
+              </Link> 
+              <Link>
                 <Icon icon="fluent:delete-24-regular" />
-              </td>
-            </tr>
-          </tbody>
-        </Table>
+              </Link>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
