4fac52f5933c502866b1b957837bdc10bdf28fe1
Sadim Mali <sadimmalakar77@gmail.com>
2024-04-19 20:31:55 +0545

    feat: Add modal to confirm deletion of item

--- client/src/components/ViewItem.jsx
+++ client/src/components/ViewItem.jsx
@@ -1,8 +1,17 @@
 import Table from "react-bootstrap/Table";
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 export default function ViewItem() {
+  const [show, setShow] = useState(false);
+
+  const handleModal = () => {
+    setShow(!show);
+  };
+
   return (
     <div>
       <div className=" d-flex justify-content-between w-100 mb-3">
@@ -33,20 +42,37 @@ export default function ViewItem() {
             </td>
             <td>
               <div className="d-flex gap-2">
-              <Link to={`/user/view-item/dfaf`}>
-              <Icon icon="lets-icons:view-alt" />
-              </Link> 
-              <Link>
-                <Icon icon="tabler:edit" />
-              </Link> 
-              <Link>
-                <Icon icon="fluent:delete-24-regular" />
-              </Link>
+                <Link to={`/user/view-item/dfaf`}>
+                  <Icon icon="lets-icons:view-alt" />
+                </Link>
+                <Link>
+                  <Icon icon="tabler:edit" />
+                </Link>
+                <Link>
+                  <Icon icon="fluent:delete-24-regular" onClick={handleModal} />
+                </Link>
               </div>
             </td>
           </tr>
         </tbody>
       </Table>
+
+      {/* modal */}
+      <Modal
+        show={show}
+        onHide={handleModal}
+        backdrop="static"
+        keyboard={false}
+        centered
+      >
+        <Modal.Body>Do you want to delete the item?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleModal}>
+            No
+          </Button>
+          <Button variant="danger">Yes</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
