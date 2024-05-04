481ac4f158f774df6a237abd660068ad867fec0b
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-04 21:31:49 +0545

    feat: Export array of object for inputField as utlis

---
+++ client/src/utlis/Utlis.js
@@ -0,0 +1,31 @@
+export const addInputField = [
+  {
+    id: 1,
+    type: "text",
+    placeholder: "What was lost",
+    label: "What was lost",
+  },
+  {
+    id: 2,
+    type: "text",
+    placeholder: "Category",
+    label: "Category",
+  },
+  {
+    id: 3,
+    type: "text",
+    placeholder: "Location",
+    label: "Where was lost?",
+  },
+  {
+    id: 3,
+    type: "date",
+    label: "Date lost lost?",
+  },
+  {
+    id: 4,
+    type: "text",
+    placeholder: "Additional Info",
+    label: "Additional Information",
+  },
+];
