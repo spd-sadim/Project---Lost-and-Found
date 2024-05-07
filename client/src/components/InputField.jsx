34ca581143ae8313e852564ffe4126ad639774aa
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-07 22:02:09 +0545

    feat: Add onChange fn props to InputField component

--- client/src/components/InputField.jsx
+++ client/src/components/InputField.jsx
@@ -1,13 +1,15 @@
-export default function InputField({ label, type, placeholder }) {
+export default function InputField({ label,name, type, placeholder, handleChange }) {
   return (
     <div className="form-group">
-      <label htmlFor="" className="d-flex flex-column mb-2">
+      <label htmlFor="" className="d-flex flex-column fw-bold mb-2">
         {label}
       </label>
       <input
         type={type}
+        name={name}
         className=" w-100 border rounded p-2"
         placeholder={placeholder}
+        onChange={handleChange}
       />
     </div>
   );
