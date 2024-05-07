9a674da8058dea4275d1d590c4228cc72bae9e70
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-07 21:59:21 +0545

    feat: Create array of object to dynamic render inputFields

---
+++ client/src/utils/utils.js
@@ -0,0 +1,90 @@
+export const lostInputField = [
+    {
+      id: 1,
+      type: "text",
+      name: 'name',
+      placeholder: "What was lost?",
+      label: "What was lost",
+    },
+   
+    {
+      id: 2,
+      type: "text",
+      name: 'location',
+      placeholder: "Location",
+      label: "Where was lost?",
+    },
+    {
+      id: 3,
+      type: "date",
+      name: 'date',
+      label: "Date lost",
+    },
+    {
+      id: 4,
+      type: "text",
+      name: 'additionalInfo',
+      placeholder: "Additional Info",
+      label: "Additional Information",
+    },
+  ];
+  export const foundInputField = [
+    {
+      id: 1,
+      type: "text",
+      name: 'name',
+      placeholder: "What was found?",
+      label: "What was found",
+    },
+    {
+      id: 2,
+      type: "text",
+      name: 'location',
+      placeholder: "Location",
+      label: "Where was found?",
+    },
+    {
+      id: 3,
+      type: "date",
+      name: 'date',
+      label: "Date found",
+    },
+    {
+      id: 4,
+      type: "text",
+      name: 'additionalInfo',
+      placeholder: "Additional Info",
+      label: "Additional Information",
+    },
+  ];
+  
+  export const personInputField = [
+    {
+      id: 1,
+      type: "number",
+      name: 'age',
+      placeholder: "Age",
+      label: "Age",
+    },
+    {
+      id: 2,
+      type: "text",
+      name: 'fatherName',
+      placeholder: "Father's Name",
+      label: "Father's Name",
+    },
+    {
+      id: 2,
+      type: "text",
+      name: 'motherName',
+      placeholder: "Mother's Name",
+      label: "Mother's Name",
+    },
+    {
+      id: 3,
+      type: "text",
+      name: 'skinColor',
+      placeholder: "Skin color",
+      label: "Skin color",
+    },
+  ]
