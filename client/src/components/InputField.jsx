01b142217af0c287f46a7908ca33442462fdb8ce
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-14 21:47:43 +0545

    refactor: label for  InputField.jsx

--- client/src/components/InputField.jsx
+++ client/src/components/InputField.jsx
@@ -1,12 +1,13 @@
 export default function InputField({ label,name, type, placeholder, handleChange }) {
   return (
     <div className="form-group">
-      <label htmlFor="" className="d-flex flex-column fw-bold mb-2">
+      <label htmlFor={name} className="d-flex flex-column fw-bold mb-2">
         {label}
       </label>
       <input
         type={type}
         name={name}
+        id={name}
         className=" w-100 border rounded p-2"
         placeholder={placeholder}
         onChange={handleChange}
