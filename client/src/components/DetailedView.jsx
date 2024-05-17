2f4279ffa48fdfcd0bb170017bf33bc679c60476
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-18 00:02:54 +0545

    feat: Integrate api to fetch post for detail view

--- client/src/components/DetailedView.jsx
+++ client/src/components/DetailedView.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import image from "../assets/samsung.jpg";
 import { useParams } from "react-router";
 import { Icon } from "@iconify-icon/react";
+import axios from "axios";
+import { useSearchParams } from "react-router-dom";
 
 const InfoItem = ({ icon, label, value }) => (
   
@@ -24,20 +26,32 @@ const InfoItem = ({ icon, label, value }) => (
 
 export default function DetailedView() {
   let { id } = useParams();
+  const [searchParams] = useSearchParams();
+    const type = searchParams.get("type");
+    console.log(type);
   const font = {
     fontSize: "0.9rem",
   };
 
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    // Fetch item details using the ID
+    axios.get(`/api/${type}/view/${id}`)
+      .then(response => {
+        setItem(response.data[0]);
+      })
+      .catch(error => {
+        console.error('Error fetching item details:', error);
+      });
+  }, []);
+console.log(item[0]);
   return (
     <div>
       <Container>
         <Row>
           <Col lg={4} md={5}>
-            <img src={image} alt="item image" className="img-fluid rounded" />
-            <h4>Person's contact</h4>
-            <p>Name : Sadim Mali</p>
-            <p>phone : +123 456 789</p>
-            <p>Location: Santa Rose Beach, Los Angeles, CA</p>
+            <img src={`http://localhost:3000/Images/${item.image}`} alt="item image" className="img-fluid rounded" />
           </Col> 
           <Col lg={8} md={7}>
             <button>status</button>
@@ -46,23 +60,21 @@ export default function DetailedView() {
               <span>Item details</span>
             </h5>
 
-            <InfoItem icon="gg:nametag" label="Name" value={"Item"} />
-            <InfoItem icon="tabler:category-filled" label="Category" value={"This is category"} />
-            <InfoItem icon="system-uicons:location" label="Location" value={"Kirtipur - 07, Kathmandu"} />
-            <InfoItem icon="lets-icons:date-today-light" label="Date" value={"2024 - 04 - 11"} />
+            <InfoItem icon="gg:nametag" label="Name" value={item.item_name} />
+            <InfoItem icon="tabler:category-filled" label="Category" value={item.category} />
+            <InfoItem icon="system-uicons:location" label="Location" value={item.location} />
+            <InfoItem icon="lets-icons:date-today-light" label="Date" value={item.date} />
 
           <div className="description shadow-lg bg-sec p-3 mt-5 border rounded ">
             <h5 className="d-flex align-items-center gap-2 font-primary fw-bold">
               {" "}
-              <span>Description </span> 
+              <span>Additional Info </span> 
               <Icon icon="ph:puzzle-piece-light" width="25" height="25" />{" "}
             </h5>
 
             <p>
               {" "}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
-              perferendis! Odio labore rerum adipisci autem. Totam quos
-              perferendis nostrum obcaecati, saepe facilis amet excepturi.
+              {item.additional_info}
             </p>
           </div>
           </Col>
@@ -71,4 +83,3 @@ export default function DetailedView() {
     </div>
   );
 }
-5;
