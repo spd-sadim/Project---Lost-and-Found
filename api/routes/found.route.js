477d392c7a3b9d9295eb88eb6ccdefe7d7088cb8
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-17 23:59:37 +0545

    feat: Add routes for found

--- api/routes/found.route.js
+++ api/routes/found.route.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { createFoundPost, getFoundPost } from "../controllers/found.controller.js";
+import { createFoundPost, getAllFoundPost, getFoundPostById, getFoundPostByUserId  } from "../controllers/found.controller.js";
+import upload from "../utils/multer.js";
 
 
 const router = express.Router();
 
-router.get("/", getFoundPost);
-router.post("/create", createFoundPost);
+router.get("/", getAllFoundPost);
+router.post("/create", upload.single('image'), createFoundPost);
+router.get("/:id", getFoundPostByUserId);
+router.get("/view/:id", getFoundPostById);
+
 
 export default router;
