829e9a61fcdf97ef470ab40b995d5c4d9bef5ad8
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-19 23:27:50 +0545

    style: Modify hero area height and maxWidth of select option in hero.jsx

--- client/src/components/Hero/HeroForm.jsx
+++ client/src/components/Hero/HeroForm.jsx
@@ -6,14 +6,14 @@ export default function HeroForm() {
   const [categoryValue, setCategoryValue] = useState("all");
 
   return (
-    <div className="px-lg-5">
+    <div className="px-lg-5 d-flex w-100 justify-content-center">
 
-    <div className="d-flex px-lg-5 w-100" >
+    <div className="d-flex px-lg-5 w-100" style={{maxWidth: "75%"}}>
       {/* <label>What was lost or found *</label>
       <input type="text" name="itemName" id="itemName" className="inputField" /> */}
       {/* <input list="categories" placeholder="Search" /> */}
       {/* <datalist id="categories" className="h-50"> */}
-      <select name="category" className="p-2 w-100" onChange={(e)=> { setCategoryValue(e.target.value)}}>
+      <select name="category" className="p-2 w-100"  onChange={(e)=> { setCategoryValue(e.target.value)}}>
       <option defaultValue={"all"}>All category</option>
         {categories.map((category, index) => (
           <option value={index+1} key={index}> {category} </option>
