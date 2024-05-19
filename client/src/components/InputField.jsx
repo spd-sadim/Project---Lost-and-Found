c9b382d9bcfdc55adf2f4e93c4bd8fffaca0b551
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-19 23:34:27 +0545

    feat: Add value attribute in input field

--- client/src/components/InputField.jsx
+++ client/src/components/InputField.jsx
@@ -1,4 +1,4 @@
-export default function InputField({ label,name, type, placeholder, handleChange }) {
+export default function InputField({ label,name, type, placeholder, handleChange, value }) {
   return (
     <div className="form-group">
       <label htmlFor={name} className="d-flex flex-column fw-bold mb-2">
@@ -8,6 +8,7 @@ export default function InputField({ label,name, type, placeholder, handleChange
         type={type}
         name={name}
         id={name}
+        value={value}
         className=" w-100 border rounded p-2"
         placeholder={placeholder}
         onChange={handleChange}
