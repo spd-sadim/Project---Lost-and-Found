e48e5089ae4dbb3ce656eb26c9a3eb1befa51465
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-19 23:10:53 +0545

    feat: Create post.controller to get all posts from db

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,42 @@
+import pool from "../db.js";
+
+export const getPosts = async(req, res)=>{
+    const { category, status, date, location, keywords } = req.query;
+
+    let query = 'SELECT * FROM lost_posts WHERE 1 = 1'; // Base query for lost posts
+
+    if (category) query += ` AND category = '${category}'`;
+    if (status) query += ` AND status = '${status}'`;
+    if (date) query += ` AND date = '${date}'`;
+    if (location) query += ` AND location ILIKE '%${location}%'`;
+    if (keywords) query += ` AND description ILIKE '%${keywords}%'`;
+
+    try {
+        res.send(req.query)
+        const lostPosts = await pool.query(query);
+        
+        // Repeat the process for found posts and merge the results
+        // (Alternatively, UNION ALL can be used if both tables have the same structure)
+        const foundPostsQuery = query.replace('lost_posts', 'found_posts');
+        const foundPosts = await pool.query(foundPostsQuery);
+
+        const allPosts = [...lostPosts.rows, ...foundPosts.rows];
+        res.json(allPosts);
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+        res.status(500).send(error.message);
+    }
+}
+
+
+export const getAllPost = async(req, res)=>{
+    try {
+        const foundPost = await pool.query('SELECT * FROM found_posts');
+        const lostPost = await pool.query('SELECT * FROM lost_posts');
+        // const allPost = {foundPost}
+        const allPost = [...foundPost.rows, ...lostPost.rows];
+        res.status(200).json(allPost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
