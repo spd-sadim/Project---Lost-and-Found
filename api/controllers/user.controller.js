3491a47898304e43e10c730ab5448c46b37e322a
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-19 23:15:48 +0545

    feat: Update user profile

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -9,3 +9,30 @@ export const getUsers = async(req, res)=>{
        }
         // res.json({message: "hello"});
 }
+
+export const updateUser = async(req, res)=>{
+        const {id, user_firstname, user_lastname, user_phonenumber, user_email} = req.body;
+        const query = `
+        UPDATE users
+        SET user_firstname = $1,
+            user_lastname = $2,
+            user_phonenumber = $3,
+            user_email = $4
+        WHERE user_id = $5
+        RETURNING *;
+      `;
+//       const  number = parseInt(user_phonenumber, 10); 
+      const values = [ user_firstname, user_lastname, user_phonenumber, user_email, id];
+        console.log(values)
+        try {
+              const response = await pool.query(query, values);
+                console.log(response.rows);
+              if (response.rows.length === 0) {
+                return res.status(404).json({ error: 'User not found' });
+              }
+
+              res.status(200).json({message: 'Profile updated succesfully', user: response.rows[0]});
+        } catch(err){
+                console.log(err);
+        }
+}
