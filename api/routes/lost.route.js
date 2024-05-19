fc99a4174feef162abe378e9702f0e08389404ea
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-19 23:11:48 +0545

    feat: Implement route for view lost post by id

--- api/routes/lost.route.js
+++ api/routes/lost.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import upload from "../utils/multer.js";
-import { createLostPost, getLostPost, getLostPostByUserId } from "../controllers/lost.controller.js";
+import { createLostPost, getLostPost, getLostPostById, getLostPostByUserId } from "../controllers/lost.controller.js";
 
 
 const router = express.Router();
@@ -9,5 +9,6 @@ const router = express.Router();
 router.get("/", getLostPost);
 router.get("/:id", getLostPostByUserId);
 router.post("/create", upload.single('image'), createLostPost);
+router.get("/view/:id", getLostPostById);
 
 export default router;
