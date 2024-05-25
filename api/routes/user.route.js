4ce144f0ad8a353881fc6e3cb203f4447b78a37d
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-25 22:44:40 +0545

    feat: Create routes for update, delete and retrieve user information by id

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { getUsers, updateUser } from "../controllers/user.controller.js";
+import {
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  updateUserByAdmin,
+} from "../controllers/user.controller.js";
+import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
-router.put("/profile", updateUser)
+router.get("/:id", getUserById);
+router.delete("/:id",verifyUser, deleteUser);
+router.put("/profile", verifyUser, updateUser);
+router.put("/:id",verifyUser, updateUserByAdmin)
 
 export default router;
