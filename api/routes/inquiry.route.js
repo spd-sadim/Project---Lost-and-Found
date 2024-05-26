3bfc8495819c9d63b16d372661ee52ecd5a8c711
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-26 21:53:55 +0545

    refactor(routes): Modify routes to get and post inquiry

--- api/routes/inquiry.route.js
+++ api/routes/inquiry.route.js
@@ -3,7 +3,7 @@ import { getInquiry, postInquiry } from "../controllers/inquiry.controller.js";
 
 const router = express.Router();
 
-router.post("/post", postInquiry);
-router.get("/get", getInquiry);
+router.post("/:id", postInquiry);
+router.get("/", getInquiry);
 
 export default router;
