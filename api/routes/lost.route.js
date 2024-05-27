20aee855258e2e45338982018fc05d7f49c82ac6
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-27 21:43:42 +0545

    feat(lost): Add route to update post by ID endpoint

--- api/routes/lost.route.js
+++ api/routes/lost.route.js
@@ -1,7 +1,8 @@
 import express from "express";
 
 import upload from "../utils/multer.js";
-import { createLostPost, deleteLostPost, getLostPost, getLostPostById, getLostPostByUserId } from "../controllers/lost.controller.js";
+import { createLostPost, deleteLostPost, getLostPost, getLostPostById, getLostPostByUserId, updateLostPostById } from "../controllers/lost.controller.js";
+import { verifyUser } from "../utils/verifyUser.js";
 
 
 const router = express.Router();
@@ -11,5 +12,6 @@ router.get("/:id", getLostPostByUserId);
 router.post("/create", upload.single('image'), createLostPost);
 router.delete("/delete/:id", deleteLostPost);
 router.get("/view/:id", getLostPostById);
+router.put("/update/:id",upload.single('image'), verifyUser, updateLostPostById);
 
 export default router;
