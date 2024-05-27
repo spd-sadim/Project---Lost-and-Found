ac66f1503f75175da48ede43538ed5539a2895e2
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-27 21:42:10 +0545

    feat(inquiry): Route to get inquiry by id

--- api/routes/inquiry.route.js
+++ api/routes/inquiry.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getInquiry, postInquiry } from "../controllers/inquiry.controller.js";
+import { getInquiry, getInquiryById, postInquiry } from "../controllers/inquiry.controller.js";
 
 const router = express.Router();
 
 router.post("/:id", postInquiry);
 router.get("/", getInquiry);
+router.get("/:id", getInquiryById);
 
 export default router;
