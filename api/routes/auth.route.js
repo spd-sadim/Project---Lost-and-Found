d5dde204cbcc958d383b6c38db6f8d911d16fad7
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-28 21:54:17 +0545

    feat(auth.route): Route to implement signout functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signIn, signUp } from "../controllers/auth.controller.js";
+import { signIn, signUp, signout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
+router.get('/signout', signout );
 
 export default router;
