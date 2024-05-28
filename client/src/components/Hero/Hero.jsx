8a9a0f5e8b5498d53fb53ad0cd238935ca84c348
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-28 21:58:59 +0545

    style: align hero content  center to its div

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ import HeroForm from "./HeroForm";
 export default function Hero() {
   return (
     <section className="hero position-relative">
-      <Container className="h-100">
-        <div className="hero-wrapper h-100 py-5 position-relative ">
+      <Container className="h-100 d-flex align-items-center justify-content-center ">
+        <div className="hero-wrapper py-5 position-relative ">
           <div className="hero-content text-center py-5">
             <h1>Reuniting lost belongings with their owners.</h1>
             <p className="font-sec">
