cd1cce3e7a0e9d267c786968fcabb8210381c374
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-28 21:59:58 +0545

    style: Add utility style className to element

--- client/src/components/Hero/HeroForm.jsx
+++ client/src/components/Hero/HeroForm.jsx
@@ -8,19 +8,19 @@ export default function HeroForm() {
   return (
     <div className="px-lg-5 d-flex w-100 justify-content-center">
 
-    <div className="d-flex px-lg-5 w-100" style={{maxWidth: "75%"}}>
+    <div className="d-flex px-lg-5 w-100 mw-100">
       {/* <label>What was lost or found *</label>
       <input type="text" name="itemName" id="itemName" className="inputField" /> */}
       {/* <input list="categories" placeholder="Search" /> */}
       {/* <datalist id="categories" className="h-50"> */}
-      <select name="category" className="p-2 w-100"  onChange={(e)=> { setCategoryValue(e.target.value)}}>
+      <select  name="category" className="p-2 w-100 selectOptions"  onChange={(e)=> { setCategoryValue(e.target.value)}}>
       <option defaultValue={"all"}>All category</option>
         {categories.map((category, index) => (
           <option value={index+1} key={index}> {category} </option>
         ))}
       </select>
-      <Link to={`/view-item?category=${categoryValue}`} className="w-25 h-100">
-          <button className="text-white myBtn-primary py-2  w-100"> Search</button>
+      <Link to={`/view-post?category=${categoryValue}`} className="w-25 h-100">
+          <button className="text-white myBtn-primary py-2  w-100 h-100"> Search</button>
       </Link>
     </div>
     </div>
