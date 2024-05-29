356caad855261f7059e27d5b04872db568a58c82
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-29 21:49:20 +0545

    feat: Add verifyAdmin middleware

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -6,14 +6,14 @@ import {
   deleteUser,
   updateUserByAdmin,
 } from "../controllers/user.controller.js";
-import { verifyUser } from "../utils/verifyUser.js";
+import { verifyAdmin, verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
-router.get("/:id", getUserById);
-router.delete("/:id",verifyUser, deleteUser);
+router.get("/:id",verifyAdmin, getUserById);
+router.delete("/:id",verifyAdmin, deleteUser);
 router.put("/profile", verifyUser, updateUser);
-router.put("/:id",verifyUser, updateUserByAdmin)
+router.put("/:id", verifyAdmin, updateUserByAdmin)
 
 export default router;
