dd08f1521f585ff6b3cfb0782bcebefdd191c01e
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-29 21:46:26 +0545

    feat(inquiry): Implement route to deleteinquiryById

--- api/routes/inquiry.route.js
+++ api/routes/inquiry.route.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { getInquiry, getInquiryById, postInquiry } from "../controllers/inquiry.controller.js";
+import {
+  deleteInquiryById,
+  getInquiry,
+  getInquiryById,
+  postInquiry,
+} from "../controllers/inquiry.controller.js";
 
 const router = express.Router();
 
 router.post("/:id", postInquiry);
 router.get("/", getInquiry);
 router.get("/:id", getInquiryById);
+router.delete("/:id", deleteInquiryById);
 
 export default router;
