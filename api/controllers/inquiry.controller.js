3f8ad0f650f413d1c90a57aa7a62dca850ac0152
Sadim Mali <sadimmalakar77@gmail.com>
2024-05-29 21:47:55 +0545

    feat(inquiry): Implement delete inquiry from the db

--- api/controllers/inquiry.controller.js
+++ api/controllers/inquiry.controller.js
@@ -16,7 +16,7 @@ export const postInquiry = async (req, res, next) => {
   }
 };
 
-//get all inquiry 
+//get all inquiry
 export const getInquiry = async (req, res, next) => {
   try {
     const result = await pool.query("SELECT * FROM inquirylist");
@@ -30,12 +30,26 @@ export const getInquiry = async (req, res, next) => {
 
 //get inquriy by id
 export const getInquiryById = async (req, res, next) => {
-  const {id} = req.params;
+  const { id } = req.params;
   try {
-    const result = await pool.query("SELECT * FROM inquirylist WHERE id = $1", [id]);
+    const result = await pool.query("SELECT * FROM inquirylist WHERE id = $1", [
+      id,
+    ]);
     if (result.rows.length === 0)
       return next(errorHandler(404, "No inquiry message"));
     res.status(200).json(result.rows[0]);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteInquiryById = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query("DELETE FROM inquirylist WHERE id = $1", [
+      id,
+    ]);
+    res.status(200).json({ message: "Inquiry message deleted successfully" });
   } catch (err) {
     next(err);
   }
