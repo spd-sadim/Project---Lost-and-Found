6bc2a4c624707914791febfbfb267f1ee32ea7de
Sadim Mali <sadimmalakar77@gmail.com>
2024-06-10 21:39:58 +0545

    style(ChangePassword): Add label instead of placeholder

--- client/src/components/dashboard/ChangePassword.jsx
+++ client/src/components/dashboard/ChangePassword.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import InputField from "../InputField";
 import FormWrapper from "./FormWrapper";
 import Wrapper from "./Wrapper";
 
 export default function ChangePassword() {
+  const [error, setError] = useState("");
   return (
     <Wrapper>
-    
-    <FormWrapper>
-    <h4>Change Password</h4>
-        <InputField type='password' placeholder='Current Password'/>
-        <InputField type='password' placeholder='New Password' />
-        <button type="submit" className="btn btn-secondary">Update Password</button>
-    </FormWrapper>
+      <FormWrapper>
+        <h4 className="py-2"> Password</h4>
+        {error && <p className="text-danger"></p>}
+        <InputField type="password" label="Current Password" />
+        <InputField type="password" label="New Password" />
+        <InputField type="password" label="Verify Password" />
+        <button type="submit" className="btn btn-secondary">
+          Update Password
+        </button>
+      </FormWrapper>
     </Wrapper>
-  )
+  );
 }
