0284ac9aea9e9b1ce726fbdeafe7d18d8b2ab0ce
Sadim Mali <sadimmalakar77@gmail.com>
2024-06-13 22:58:40 +0545

    feat(user.route): Implement route to change-password

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,15 +5,17 @@ import {
   updateUser,
   deleteUser,
   updateUserByAdmin,
+  changePassword,
 } from "../controllers/user.controller.js";
 import { verifyAdmin, verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
-router.get("/:id",verifyAdmin, getUserById);
-router.delete("/:id",verifyAdmin, deleteUser);
+router.get("/:id", verifyUser, verifyAdmin, getUserById);
+router.delete("/:id", verifyAdmin, deleteUser);
 router.put("/profile", verifyUser, updateUser);
-router.put("/:id", verifyAdmin, updateUserByAdmin)
+router.put("/:id", verifyAdmin, updateUserByAdmin);
+router.post("/change-password",verifyUser, changePassword);
 
 export default router;
