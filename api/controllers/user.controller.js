189d84ec2cfca308ee92ab9ab2067407f6067252
Sadim Mali <sadimmalakar77@gmail.com>
2024-06-13 22:59:28 +0545

    feat(changePasword): Implement user password change functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,9 +1,13 @@
 import pool from "../db.js";
 import { errorHandler } from "../utils/error.js";
+import bcrypt from "bcryptjs";
 
 export const getUsers = async (req, res, next) => {
   try {
-    const result = await pool.query("SELECT * FROM users WHERE NOT user_id = $1", [req.query.id]);
+    const result = await pool.query(
+      "SELECT * FROM users WHERE NOT user_id = $1",
+      [req.query.id]
+    );
     const santizedUsers = santizedUserData(result.rows);
     res.json(santizedUsers);
   } catch (err) {
@@ -32,17 +36,17 @@ export const getUserById = async (req, res) => {
   }
 };
 
-
-
 //delete user
-export const deleteUser = async (req,res, next)=>{
+export const deleteUser = async (req, res, next) => {
   try {
-    const result = pool.query('DELETE FROM users WHERE user_id = $1', [req.params.id]);
-    res.status(200).json({message: "User deleted succesfully"});
-  } catch(err){
+    const result = await pool.query("DELETE FROM users WHERE user_id = $1", [
+      req.params.id,
+    ]);
+    res.status(200).json({ message: "User deleted succesfully" });
+  } catch (err) {
     next(err);
   }
-}
+};
 
 //update user
 export const updateUser = async (req, res, next) => {
@@ -112,5 +116,39 @@ export const updateUserByAdmin = async (req, res, next) => {
       .json({ message: "Profile updated succesfully", user: userData });
   } catch (err) {
     next(errorHandler(401, err));
+  }
+};
+
+//change password
+export const changePassword = async (req, res, next) => {
+  const { currentPassword, newPassword } = req.body;
+  try {
+    const userQuery =
+      "SELECT user_id, user_password FROM users WHERE user_id = $1";
+    const userResult = await pool.query(userQuery, [req.user.id.user_id]);
+
+    if (userResult.rows.length === 0) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    const user = userResult.rows[0];
+
+    const isCurrentPassword = bcrypt.compareSync(
+      currentPassword,
+      user.user_password
+    );
+    if (!isCurrentPassword)
+      return next(errorHandler(401, "Current password is incorrect"));
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    const updateQuery =
+      "UPDATE users SET user_password = $1 WHERE user_id = $2";
+
+    await pool.query(updateQuery, [hashedPassword, user.user_id]);
+
+    res.status(201).json({ message: "Password updated succesfully" });
+  } catch (err) {
+    next(err);
   }
 };
