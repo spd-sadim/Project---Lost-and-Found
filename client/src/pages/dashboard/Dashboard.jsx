f48d3b26f053adfbf092c24387f5b8eda539e773
Sadim Mali <sadimmalakar77@gmail.com>
2024-06-26 00:04:09 +0545

    feat: Add dropdown quick access in dashboard

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,15 +1,31 @@
 import { Icon } from "@iconify-icon/react";
-import { useState } from "react";
-import { Outlet } from "react-router";
+import { useContext, useState } from "react";
+import { Outlet, useNavigate } from "react-router";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import { Dropdown } from "react-bootstrap";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
 
-export default function Dashboard({ type }) {
+export default function Dashboard() {
   const [isCollapse, setIsCollapse] = useState(false);
+  const {user, dispatch} = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get('/api/auth/signout');
+      dispatch({ type: "logout" });
+      localStorage.removeItem("user");
+      navigate('/login');
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="wrapper d-flex" style={{ backgroundColor: "#fafafa" }}>
-      <Sidebar isCollapse={isCollapse} userType={type} />
+      <Sidebar isCollapse={isCollapse}  />
       <main className="w-100">
-        <nav className="bg-white">
+        <nav className="bg-white sticky-top" style={{boxShadow: "0 0 2rem 0 rgba(41, 48, 66, .1)"}}>
           <div className="container d-flex align-items-center justify-content-between">
             <Icon
               icon="jam:menu"
@@ -19,7 +35,17 @@ export default function Dashboard({ type }) {
               }}
             />
             <div className="user p-2">
-            <Icon icon="guidance:user-1" style={{fontSize: "2.5rem"}} />
+            <Dropdown>
+      <Dropdown.Toggle split variant="white" id="dropdown-basic" className="d-flex align-items-center justify-content-center">
+           <Icon icon="guidance:user-1" style={{fontSize: "2.5rem"}} /> <span className="px-2">{user.user_firstname + " " + user.user_lastname} </span>  
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href={`/${user.role}/profile`}>My Profile</Dropdown.Item>
+        <Dropdown.Item  href={`/${user.role}/change-password`}>Change password</Dropdown.Item>
+        <Dropdown.Item onClick={handleLogout} className="text-danger">Log out</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
             </div>
           </div>
         </nav>
